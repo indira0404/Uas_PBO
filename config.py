fc0ad310cb0260221f09e522c39ccce7029e4170
indira0404 <indiraazalia044@gmail.com>
2025-06-12 16:52:50 +0700

    ini yang bagian bacendnya ya

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    MYSQL_HOST = '127.0.0.1'
+    MYSQL_PORT = 3306
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'zakat_db'
+    SECRET_KEY = ''  # Optional, for additional Flask features
